2921802fef4dfd563bbf2a59a4464c7b0adf8d7d
NatalyBMota <interactivedesign@yahoo.com>
2023-07-21 17:14:17 -0500

    Added spacing between the content about each individual movie; first by creating a line break; or br element; between each, using JavaScript. Then, by wrapping this content in a div and giving this div a class name using JavaScript, then adding margin to the bottom of this div using CSS.

--- js/graphQL_API.js
+++ js/graphQL_API.js
@@ -20,34 +20,25 @@ let queryString = `{
 				species {
 				  name 
 				  classification 
-				  homeworld {
-					name
-				  }
 				}
 			}
 		}
 	}
 }`;
 
-fetch(`https://swapi-graphql.netlify.app/.netlify/functions/index?query=${queryString}`)
-.then(function(response) {
-	if (response.status == 200) {
-		return response.json();
-	}
-	else {
-		console.log("Whoops! There is a problem with connecting to the API! The problem is likely an error in the URL of the request.");
-	}
-})
-.then(function(jsonData) {
+function populateWebPageWithServerData(jsonData) {
 	const mainDiv = document.querySelector('.main');
 	const filmsArr = jsonData.data.allFilms.films;
-	console.log(filmsArr[0]);
+	console.log(filmsArr);
 
 	for (let i = 0; i < filmsArr.length; i++) {
+		let divFilm = document.createElement('div');
+		divFilm.className = "individualFilm";
+		mainDiv.appendChild(divFilm);
 		const filmTitle = filmsArr[i].title;
 		const h2 = document.createElement('h2');
 		h2.innerText = `${filmTitle}`;
-		mainDiv.appendChild(h2);
+		divFilm.appendChild(h2);
 
 		const director = filmsArr[i].director;
 		const h3 = document.createElement('h3');
@@ -59,7 +50,7 @@ fetch(`https://swapi-graphql.netlify.app/.netlify/functions/index?query=${queryS
 		h3.style.fontWeight = 'normal';
 		directorSpan.style.fontWeight = 'bold';
 
-		mainDiv.appendChild(h3);
+		divFilm.appendChild(h3);
 		const releaseDate = filmsArr[i].releaseDate;
 		const convertedReleaseDate = convertToFriendlyDateFormat(releaseDate);
 		const h4 = document.createElement('h4');
@@ -69,13 +60,57 @@ fetch(`https://swapi-graphql.netlify.app/.netlify/functions/index?query=${queryS
 		const txtNodeWithReleaseDate = document.createTextNode(`${convertedReleaseDate}`);
 		releaseDateSpan.appendChild(txtNodeWithReleaseDate);
 		h4.appendChild(releaseDateSpan);
-		//h4.innerHTML = `${releaseDate}`;
-		mainDiv.appendChild(h4);
+		divFilm.appendChild(h4);
 		releaseDateSpan.style.fontWeight = 'bold';
-		
-		
+
+		const speciesArr = filmsArr[i].speciesConnection.species;
+		let ul = document.createElement('ul');
+		let h4Species = document.createElement('h4');
+		h4Species.innerText = "Species:";
+		divFilm.appendChild(h4Species);
+		divFilm.appendChild(ul);
+		for (let i = 0; i < speciesArr.length; i++) {
+			const species = speciesArr[i];
+			const speciesName = species.name;
+			let li = document.createElement('li');
+			let spanSpeciesName = document.createElement('span');		
+			spanSpeciesName.innerText = "Name: ";
+			spanSpeciesName.style.fontWeight = 'bold';
+			li.appendChild(spanSpeciesName);
+			ul.appendChild(li);
+			const txtNodeWithSpeciesName = document.createTextNode(`${speciesName}`);
+			li.appendChild(txtNodeWithSpeciesName);
+
+			const classification = species.classification;
+			let ulClassification = document.createElement('ul');
+			let liClassification = document.createElement('li');
+			let spanClassification = document.createElement('span');
+			spanClassification.innerText = "Classification: ";
+			spanClassification.style.fontWeight = 'bold';
+			liClassification.appendChild(spanClassification);
+			let txtNodeClassification = document.createTextNode(`${classification}`);
+			liClassification.appendChild(txtNodeClassification);
+			//liClassification.innerText = `${classification}`;
+			li.appendChild(ulClassification);
+			ulClassification.appendChild(liClassification);
+		}
+
+		let br = document.createElement('br');
+		divFilm.appendChild(br);
 	}
-	
+}
+
+fetch(`https://swapi-graphql.netlify.app/.netlify/functions/index?query=${queryString}`)
+.then(function(response) {
+	if (response.status == 200) {
+		return response.json();
+	}
+	else {
+		console.log("Whoops! There is a problem with connecting to the API! The problem is likely an error in the URL of the request.");
+	}
+})
+.then(function(jsonData) {
+	populateWebPageWithServerData(jsonData);	
 })
 .catch(function(error) {
 	console.log("There was a problem with getting data from the API in JSON format.", error);
